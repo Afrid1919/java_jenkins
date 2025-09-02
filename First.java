49bdf3dba9b6e732d8cef4c7c3261375ec178546
afrid1919 <afridpathan1908@gmail.com>
2025-09-02 12:20:44 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class First{
+    public static void main(String[] args) {
+        System.out.println("Welcome to DevOps...");
+        System.out.println("Learning jenkins with java");
+        System.out.println(new Date());
+    }
+}
